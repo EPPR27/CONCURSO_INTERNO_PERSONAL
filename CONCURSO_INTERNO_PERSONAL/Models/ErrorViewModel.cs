5cda8815e33640c5583b13465cab2dd45a2cf0cb
Eddie Paz Reyes <eddie230@hotmail.com>
2023-10-10 17:56:35 -0500

    Agregar archivos de proyecto.

---
+++ CONCURSO_INTERNO_PERSONAL/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CONCURSO_INTERNO_PERSONAL.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
